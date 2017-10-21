1257cb440ec3ffcf4ad7c5e53821c7a52b700a47
Keith Haber <kjhaber@gmail.com>
2017-10-20 22:09:44 -0700

    Update 'tm' tmux script to work within session
    The tm script provides a shortcut for creating a tmux session if it
    doesn't already exist, and attaching to a session if it does.  It's set
    up with autocomplete for the session list as well.
    
    However, to create or switch between sessions while within a session,
    it's been necessary to detach from the session first.  This change
    makes it so `tm` works within a session, allowing fast session switching
    in addition to creating and attaching.

--- zsh/bin/tm.zsh
+++ zsh/bin/tm.zsh
@@ -1,11 +1,13 @@
 # tm.zsh
 #
-# Simplifies creating new tmux sessions and attaching to existing sessions.
+# Simplifies creating new tmux sessions, attaching to existing sessions,
+# and switching between sessions.
 #
 
 function tm() {
     [[ -z "$1" ]] && { echo "usage: tm <session>" >&2; return 1; }
-    tmux has -t "$1" 2> /dev/null && tmux attach -t "$1" || tmux new -s "$1"
+    [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
+    tmux has -t "$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
 }
 
 function __tmux-sessions() {
