2f5ec9329de6215025852b83f293e1701ce6ad32
Keith Haber <kjhaber@gmail.com>
2018-08-01 22:54:45 -0700

    Improve cdfzf.zsh
    Heavily refactored to exclude hidden files, validate inputs, handle
    ctrl-c from fzf better, and break commands down and comment them to
    make the script more understandable.  Also fix up most shellcheck
    warnings.

--- zsh/bin/cdfzf.zsh
+++ zsh/bin/cdfzf.zsh
@@ -1,14 +1,61 @@
 # cdfzf.zsh
 #
 # Function for creating aliases to cd into a child directory of a root path
-# using fzf fuzzy matching.  https://github.com/junegunn/fzf 
+# using fzf fuzzy matching.  https://github.com/junegunn/fzf
+#
+# Note that this needs to be a zsh function instead of a plain shell script.
+# Otherwise the script will cd to the new directory in a subshell and exit,
+# leaving you where you started.
+#
+# Arg 1: Root directory
+# Arg 2: Max subdirectory depth (default 1)
 #
 # Example:
 # cdfzf ~/Documents 2
 
 function cdfzf() {
-  ROOT_DIR=$1
-  MAX_DEPTH=$2
-  cd $ROOT_DIR/$((find $ROOT_DIR -type d -maxdepth $MAX_DEPTH ; echo) | fzf --no-multi | sed "s|^$ROOT_DIR/||g") ; pwd
+
+  # $1 is the root directory.  if path is relative to ~ shorthand, expand to $HOME
+  ROOT_DIR=$(sed -e "s|^\~\/|$HOME/|g" <<< "$1")
+
+  # if no root dir provided, exit
+  if [ -z "$ROOT_DIR" ] ; then
+    echo "Usage: cdfzf <root_dir> <max_subdir_depth>"
+    return 1
+  fi
+
+  # handle special case of '~' only
+  if [ "$ROOT_DIR" = '~' ] ; then
+    ROOT_DIR="$HOME"
+  fi
+
+  # $2 is selectable subdirectory depth.  default is 1 (seems like a sane default)
+  MAX_SUBDIR_DEPTH="$2"
+  if [ -z "$MAX_SUBDIR_DEPTH" ] ; then
+    MAX_SUBDIR_DEPTH=1
+  fi
+
+  # find list of subdirectories of ROOT_DIR.
+  # the sed command removes ROOT_DIR from the options shown during fuzzy select
+  # the bare 'echo' adds an empty element to allow selecting the root dir itself
+  ROOT_SUBDIR_LIST=$( \
+      find -L "$ROOT_DIR" -type d -maxdepth "$MAX_SUBDIR_DEPTH" | \
+      { sed -e "s|^$ROOT_DIR/||g" ; echo ; } \
+    )
+
+  # strip out hidden directories.  maybe make this conditional later
+  ROOT_SUBDIR_LIST=$( grep -v -E '(^\.)|(\/\.)' <<< "$ROOT_SUBDIR_LIST" )
+
+  # fuzzy-seelct from list of subdirectories
+  ROOT_SUBDIR=$( fzf-tmux --no-multi -r <<< "$ROOT_SUBDIR_LIST" )
+
+  # if fzf did not exit normally, don't change directory
+  if [ $? -ne 0 ] ; then
+    return 1
+  fi
+
+  # change to selected directory and output new location
+  cd "$ROOT_DIR/$ROOT_SUBDIR" || return 1
+  pwd
 }
 
