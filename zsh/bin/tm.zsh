a724bc232e66b5292fec21362110939e15ccf575
Keith Haber <kjhaber@gmail.com>
2019-11-30 08:26:43 -0800

    Select tmux session names by exact match in tm.zsh
    For most tmux commands, -t (target) argument referring to target-session
    will search for a prefix match first.  Using -t=(name) uses an exact
    match only.
    
    In one of my projects, I wanted to create multiple sessions ("spire" is
    the server/primary project, "spire-ui" is the secondary).  Once "spire"
    was open though, I couldn't create "spire-ui" -- tm.zsh would instead
    select "spire" with prefix match.  This change fixes the issue.

--- zsh/bin/tm.zsh
+++ zsh/bin/tm.zsh
@@ -7,7 +7,7 @@
 function tm() {
     [[ -z "$1" ]] && { echo "usage: tm <session>" >&2; return 1; }
     [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
-    tmux has -t "$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
+    tmux has -t="$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
 }
 
 function __tmux-sessions() {
