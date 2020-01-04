10e0875702f8bff695cf53272440f9f31d2b67a3
Keith Haber <kjhaber@gmail.com>
2020-01-04 12:28:27 -0800

    Set REPORTTIME in timeprompt.zsh
    Use the zsh REPORTTIME feature to output the execution time of commands
    run when 'timeprompt' mode is enabled.  REPORTTIME is a feature of zsh;
    when set to a number, zsh reports the execution time of commands that
    run longer than $REPORTTIME seconds (user and system time, not just
    wall-clock time).

--- zsh/bin/timeprompt.zsh
+++ zsh/bin/timeprompt.zsh
@@ -1,8 +1,10 @@
 BARE_PROMPT="$PROMPT"
 timeprompt() {
   export PROMPT="\$(date \"+%H:%M:%S\") $BARE_PROMPT"
+  export REPORTTIME=0
 }
 notimeprompt() {
   export PROMPT="$BARE_PROMPT"
+  unset REPORTTIME
 }
 
