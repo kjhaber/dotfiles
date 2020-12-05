a4e585842bbf79ce49662c2636d346439b7fdb9e
Keith Haber <kjhaber@gmail.com>
2020-12-05 13:00:20 -0800

    Update tm.zsh to list sessions when called without args
    ZSH autocomplete for tm.sh lists sessions when you type `tm <tab>` at a
    prompt, but it's handy to be able to output sessions in the terminal
    outside of a transient autocomplete box.

--- zsh/bin/tm.zsh
+++ zsh/bin/tm.zsh
@@ -1,11 +1,16 @@
 # tm.zsh
 #
 # Simplifies creating new tmux sessions, attaching to existing sessions,
-# and switching between sessions.
+# switching between sessions, and listing active sessions.
 #
 
 function tm() {
-    [[ -z "$1" ]] && { echo "usage: tm <session>" >&2; return 1; }
+    [[ -z "$1" ]] && {
+      echo "Usage: tm <session>"
+      echo "Active tmux sessions:"
+      tmux ls -F "* #{session_name}"
+      return
+    }
     [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
     tmux has -t="$1" 2> /dev/null && tmux $change -t "$1" || (TMUX= tmux new -d -s "$1" && tmux $change -t "$1")
 }
