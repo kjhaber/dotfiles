cef529bd9d1b6e5da92f897e99fb009c05188373
Keith Haber <kjhaber@gmail.com>
2023-11-11 20:02:27 -0800

    Update install.sh to fix nvim plugin update step

--- .config/meta/install/install.sh
+++ .config/meta/install/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env zsh
-
 echo 'Fetching kjhaber/config from github'
 git clone --bare https://github.com/kjhaber/config "$HOME/.config/meta/repo"
 alias gitdotfile='git --git-dir=$HOME/.config/meta/repo/ --work-tree=$HOME'
@@ -9,13 +8,13 @@ gitdotfile config --local status.showUntrackedFiles no
 
 echo 'Sourcing zsh configs to trigger install scripts'
 source "$HOME/.zshenv"
-source "$HOME/.zshrc" # trigger install scripts in .config/zsh
+source "$HOME/.zshrc"
+
+echo 'Installing neovim plugins'
+nvim --headless -u "$HOME/.config/nvim/init.vim" -i NONE +PlugInstall +qa\! -- &>/dev/null
 
 echo 'Installing tmux plugins'
 $HOME/.tmux/plugins/tpm/bin/install_plugins
-
-echo 'Installing neovim plugins'
-nvim -es -u "$HOME/.config/nvim/init.vim" -i NONE -c "PlugInstall" -c "qa"
 
 echo 'kjhaber/config setup complete.  Restarting zsh is recommended.'
 
