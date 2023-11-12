8ec4b1143a77e9120b40ab6667c8b33545c64972
Keith Haber <kjhaber@gmail.com>
2023-11-11 16:59:25 -0800

    Update install.sh to use env's zsh

--- .config/meta/install/install.sh
+++ .config/meta/install/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 echo 'Fetching kjhaber/config from github'
 git clone --bare https://github.com/kjhaber/config "$HOME/.config/meta/repo"
@@ -11,11 +11,11 @@ echo 'Sourcing zsh configs to trigger install scripts'
 source "$HOME/.zshenv"
 source "$HOME/.zshrc" # trigger install scripts in .config/zsh
 
+echo 'Installing tmux plugins'
+$HOME/.tmux/plugins/tpm/bin/install_plugins
+
 echo 'Installing neovim plugins'
 nvim -es -u "$HOME/.config/nvim/init.vim" -i NONE -c "PlugInstall" -c "qa"
-
-echo 'Installing tmux plugins'
-~/.tmux/plugins/tpm/bin/install_plugins
 
 echo 'kjhaber/config setup complete.  Restarting zsh is recommended.'
 
