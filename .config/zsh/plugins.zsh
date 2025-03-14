e964917399e5043e68f082faeb450b6606a9c92d
Keith Haber <kjhaber@gmail.com>
2025-03-13 18:57:26 -0700

    Add zsh-autoswitch-virtualenv to zsh plugins

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -2,3 +2,4 @@ zcomet load zsh-users/zsh-autosuggestions
 zcomet load zsh-users/zsh-syntax-highlighting
 zcomet load kjhaber/cdfzf.zsh
 zcomet load kjhaber/tm.zsh
+zcomet load MichaelAquilina/zsh-autoswitch-virtualenv
