569153ca06fae8c1fd5c84426f0e137ddea864da
Keith Haber <kjhaber@gmail.com>
2025-12-29 12:21:22 -0800

    Update zsh init to install tpm only when tmux already installed

--- .config/zsh/autoload/install-tmux-tpm.zsh
+++ .config/zsh/autoload/install-tmux-tpm.zsh
@@ -1,4 +1,4 @@
-if [[ ! -d  "$HOME/.tmux/plugins/tpm" ]]; then
+if [[ -x "$(which tmux)" && ! -d  "$HOME/.tmux/plugins/tpm" ]]; then
   echo "Installing tmux tpm plugin manager..."
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
   echo "Done - Use `<tmux>-I` within tmux session to install plugins"
