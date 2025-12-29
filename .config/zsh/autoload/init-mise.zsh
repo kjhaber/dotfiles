e067fe982b0946b9957342352446bf3056f7eaa5
Keith Haber <kjhaber@gmail.com>
2025-12-29 15:56:24 -0800

    Combine mise default packages for .config and .config-local

--- .config/zsh/autoload/init-mise.zsh
+++ .config/zsh/autoload/init-mise.zsh
@@ -4,11 +4,26 @@ export MISE_GLOBAL_CONFIG_FILE="$CONFIG_LOCAL_DIR/mise/config.toml"
 
 mkdir -p "$MISE_DATA_DIR"
 
+# Install mise if not already installed
 if [[ ! -f "$MISE_INSTALL_PATH" ]]; then
   echo "Installing mise"
   curl https://mise.run | sh
 fi
 
+# Set mise default package lists
+# (e.g. https://mise.jdx.dev/lang/python.html#default-python-packages )
+export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$HOME/.cache/mise/default-python-packages-merged"
+export MISE_NODE_DEFAULT_PACKAGES_FILE="$HOME/.cache/mise/default-node-packages-merged"
+export MISE_GO_DEFAULT_PACKAGES_FILE="$HOME/.cache/mise/default-go-packages-merged"
+export MISE_RUBY_DEFAULT_PACKAGES_FILE="$HOME/.cache/mise/default-ruby-packages-merged"
+
+# Combine list of packages from `~/.config/mise/default-*-packages` and
+# `~/.config-local/mise/default-*-packages` into merged files under
+# `~/.cache/mise` directory.
+"$HOME/.config/bin/mise-merge-default-packages"
+
+
+# Install node and python if not already installed
 if [[ ! -d "$MISE_DATA_DIR/installs/node" ]]; then
   # Installing node because neovim/coc.nvim requires it
   echo "Installing node latest in mise"
@@ -22,8 +37,3 @@ fi
 
 # see .config/zsh/path.zsh for `mise activate`
 
-# mise default packages
-export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$CONFIG_LOCAL_DIR/mise/default-python-packages"
-export MISE_NODE_DEFAULT_PACKAGES_FILE="$CONFIG_LOCAL_DIR/mise/default-node-packages"
-export MISE_GO_DEFAULT_PACKAGES_FILE="$CONFIG_LOCAL_DIR/mise/default-go-packages"
-export MISE_RUBY_DEFAULT_PACKAGES_FILE="$CONFIG_LOCAL_DIR/mise/default-ruby-packages"
