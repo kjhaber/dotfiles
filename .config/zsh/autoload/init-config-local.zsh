61f915b9098e05dc36c4ac55d6acc95cafe1675f
Keith Haber <kjhaber@gmail.com>
2025-12-29 12:47:38 -0800

    Rename zsh autoload scripts from install-* to init-*

--- .config/zsh/autoload/init-config-local.zsh
+++ .config/zsh/autoload/init-config-local.zsh
@@ -1,3 +1,8 @@
+if [[ ! -d  "$HOME/.config-local" ]]; then
+  echo "Creating .config-local directory"
+  mkdir -p "$HOME/.config-local"
+fi
+
 if [[ ! -d  "$HOME/.config-local/nvim/snippets" ]]; then
   echo "Creating .config-local/nvim/snippets directory"
   mkdir -p "$HOME/.config-local/nvim/snippets"
--- .config/zsh/autoload/install-vimplug.zsh
+++
@@ -1,7 +0,0 @@
-if [[ ! -f  "$HOME/.config/nvim/autoload/plug.vim" ]]; then
-  echo "Installing vimplug..."
-  curl -fLo "$HOME/.config/nvim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  echo "Done"
-fi
-
