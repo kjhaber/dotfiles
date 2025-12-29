213542717bdb6d2e452396285d036cb94d9782a3
Keith Haber <kjhaber@gmail.com>
2025-12-29 10:04:50 -0800

    Add platform-specific brew shellenv conditionally to PATH

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,5 +1,14 @@
 export PATH="$HOME/.local/bin:$PATH"
 
+# Load Homebrew into PATH and set up completions
+if [[ -f "/opt/homebrew/bin/brew" ]] then;
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
+elif [[ -f "/home/linuxbrew/.linuxbrew/bin/brew" ]] then;
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
+fi
+
 # Allow environment-specific PATH overrides
 # (on Mac/Linux, this is where homebrew is added to PATH)
 test -f "$CONFIG_LOCAL_DIR/zsh/path.zsh" && source "$CONFIG_LOCAL_DIR/zsh/path.zsh"
