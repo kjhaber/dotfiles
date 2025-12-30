b16edea215ea5b21ff7d991e0bf8a903ccc15d6e
Keith Haber <kjhaber@gmail.com>
2025-12-29 16:18:52 -0800

    Fix init-tmux-tpm.zsh: escape backticks

--- .config/zsh/autoload/init-tmux-tpm.zsh
+++ .config/zsh/autoload/init-tmux-tpm.zsh
@@ -1,5 +1,5 @@
 if [[ -x "$(which tmux)" && ! -d  "$HOME/.tmux/plugins/tpm" ]]; then
   echo "Installing tmux tpm plugin manager..."
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-  echo "Done - Use `<tmux>-I` within tmux session to install plugins"
+  echo "Done - Use \`<tmux>-I\` within tmux session to install plugins"
 fi
