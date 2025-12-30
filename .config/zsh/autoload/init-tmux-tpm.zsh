28307cb76587427317a4eb9172435074b62858c2
Keith Haber <kjhaber@gmail.com>
2025-12-29 21:22:10 -0800

    Retry install tmux plugins after tpm install

--- .config/zsh/autoload/init-tmux-tpm.zsh
+++ .config/zsh/autoload/init-tmux-tpm.zsh
@@ -2,5 +2,7 @@ if [[ -x "$(which tmux)" && ! -d  "$HOME/.tmux/plugins/tpm" ]]; then
   # install tpm plugin manager
   echo "Installing tmux tpm plugin manager..."
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+  "$HOME/.tmux/plugins/tpm/bin/clean_plugins"
+  "$HOME/.tmux/plugins/tpm/bin/install_plugins"
   echo "Done"
 fi
