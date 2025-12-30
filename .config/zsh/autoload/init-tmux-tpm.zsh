2eb558d26e6f1c768d5ba87e385f449ccee56c1c
Keith Haber <kjhaber@gmail.com>
2025-12-29 21:00:13 -0800

    Revert install tmux plugins after tpm install
    For tpm to detect tmux configs and fully initialize, tmux needs to be
    launched after tpm is installed.

--- .config/zsh/autoload/init-tmux-tpm.zsh
+++ .config/zsh/autoload/init-tmux-tpm.zsh
@@ -1,9 +1,6 @@
 if [[ -x "$(which tmux)" && ! -d  "$HOME/.tmux/plugins/tpm" ]]; then
-  echo "Installing tmux tpm plugin manager..."
   # install tpm plugin manager
+  echo "Installing tmux tpm plugin manager..."
   git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
-
-  # install tmux plugins
-  "$HOME/.tmux/plugins/tpm/bin/install_plugins"
   echo "Done"
 fi
